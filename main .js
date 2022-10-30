2b0d9d1f6a4d66543fabda8a8ff3d795ea238215
om20102 <90694347+om20102@users.noreply.github.com>
2022-10-30 20:46:19 +0530

    Add files via upload

---
+++ main .js
@@ -0,0 +1,38 @@
+var previous_result = ' ';
+
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoded);
+}
+
+function modelLoded(){
+  console.log('Model Loaded !');
+}
+
+function draw(){
+  image(video,0,0,300,300);
+  classifier.classify(video, gotResult);
+}
+
+function gotResult(error, result){
+  if (error){
+    console.error(error);
+  } else {
+   if((result[0].confidence > 0.5) && (previous_result != result[0].label)){
+    console.log(result);
+    previous_result = result[0].label;
+    var synth = window.speechSynthesis;
+    speak_data = 'Object detected is - '+result[0].label;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    document.getElementById("result_object_name").innerHTML = result[0].label;
+    document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(3);    
+   }
+  }
+}
+
+
